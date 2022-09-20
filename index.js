6d13415a409d877c7c614dbbe0c8fb762c0633af
Darko V <darko1290@gmail.com>
2022-09-20 04:51:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,8 @@ function dotaAbilities (cb) {
       return cb(err);
     }
     const abilities1 = parseKV(result.body.replace(/redirect_range_scepter/g, '"redirect_range_scepter"'));
-    const abilities2 = VDF.parse(result.body);
-    const abilities3 = valveKV.deserialize(result.body);
-    // if (abilities1) { console.log('parse-kv works'); }
-    // if (abilities2) { console.log('vdf-parser works'); }
-    // if (abilities3) { console.log('valve-kv works'); }
+    // const abilities2 = VDF.parse(result.body);
+    // const abilities3 = valveKV.deserialize(result.body);
     cb(null, abilities1);
   });
 }
@@ -27,12 +24,9 @@ function dotaItems (cb) {
     if (err) {
       return cb(err);
     }
-    const items1 = parseKV(result.body);
-    const items2 = VDF.parse(result.body);
-    const items3 = valveKV.deserialize(result.body);
-    // if (items1) { console.log('parse-kv works'); }
-    // if (items2) { console.log('vdf-parser works'); }
-    // if (items3) { console.log('valve-kv works'); }
+    const items1 = parseKV(result.body); // parse-kv doesn't parse item_trident correctly
+    // const items2 = VDF.parse(result.body);
+    // const items3 = valveKV.deserialize(result.body);
     cb(null, items1);
   });
 }
@@ -45,11 +39,8 @@ function dotaHeroes (cb) {
       return cb(err);
     }
     const heroes1 = parseKV(result.body);
-    const heroes2 = VDF.parse(result.body);
-    const heroes3 = valveKV.deserialize(result.body);
-    // if (heroes1) { console.log('parse-kv works'); }
-    // if (heroes2) { console.log('vdf-parser works'); }
-    // if (heroes3) { console.log('valve-kv works'); }
+    // const heroes2 = VDF.parse(result.body);
+    // const heroes3 = valveKV.deserialize(result.body);
     cb(null, heroes1);
   });
 }
@@ -62,11 +53,8 @@ function abilityTooltipsEnglish (cb) {
       return cb(err);
     }
     const abilitytooltips1 = parseKV(result.body.replace(/" and turn rate reduced by %dMODIFIER_PROPERTY_TURN_RATE_PERCENTAGE%%%\./g, ' and turn rate reduced by %dMODIFIER_PROPERTY_TURN_RATE_PERCENTAGE%%%."'));
-    const abilitytooltips2 = VDF.parse(result.body.replace(/" and turn rate reduced by %dMODIFIER_PROPERTY_TURN_RATE_PERCENTAGE%%%\./g, ' and turn rate reduced by %dMODIFIER_PROPERTY_TURN_RATE_PERCENTAGE%%%."'));
-    const abilitytooltips3 = valveKV.deserialize(result.body);
-    // if (abilitytooltips1) { console.log('parse-kv works'); }
-    // if (abilitytooltips2) { console.log('vdf-parser works'); }
-    // if (abilitytooltips3) { console.log('valve-kv works'); }
+    // const abilitytooltips2 = VDF.parse(result.body.replace(/" and turn rate reduced by %dMODIFIER_PROPERTY_TURN_RATE_PERCENTAGE%%%\./g, ' and turn rate reduced by %dMODIFIER_PROPERTY_TURN_RATE_PERCENTAGE%%%."'));
+    // const abilitytooltips3 = valveKV.deserialize(result.body); // valve-kv ignores text outside of quotes
     cb(null, abilitytooltips1);
   });
 }
@@ -79,12 +67,9 @@ function dotaTooltipsEnglish (cb) {
       return cb(err);
     }
     const dotatooltips1 = parseKV(result.body);
-    const dotatooltips2 = VDF.parse(result.body);
-    const dotatooltips3 = valveKV.deserialize(result.body);
-    // if (dotatooltips1) { console.log('parse-kv works'); }
-    // if (dotatooltips2) { console.log('vdf-parser works'); }
-    // if (dotatooltips3) { console.log('valve-kv works'); }
-    cb(null, dotatooltips1);
+    // const dotatooltips2 = VDF.parse(result.body);
+    // const dotatooltips3 = valveKV.deserialize(result.body);
+    cb(null, dotatooltips2);
   });
 }
 
@@ -93,28 +78,30 @@ dotaAbilities(function (err, data) {
     console.error(err);
     return;
   }
-  // console.log(data.DOTAAbilities);
+  // console.log(data.DOTAAbilities.abaddon_borrowed_time);
+  // console.log(data.DOTAAbilities.invoker_forge_spirit.AbilityValues.spirit_hp);
 });
 dotaItems(function (err, data) {
   if (err) {
     console.error(err);
     return;
   }
-  // console.log(data.DOTAAbilities);
+  // console.log(data.DOTAAbilities.item_trident);
 });
 dotaHeroes(function (err, data) {
   if (err) {
     console.error(err);
     return;
   }
-  // console.log(data.DOTAHeroes);
+  // console.log(data.DOTAHeroes.npc_dota_hero_lion);
 });
 abilityTooltipsEnglish(function (err, data) {
   if (err) {
     console.error(err);
     return;
   }
-  // console.log(data.lang.Tokens);
+  // console.log(data.lang.Tokens.DOTA_Tooltip_modifier_nevermore_shadowraze_debuff_description);
+  // console.log(data.lang.Tokens.DOTA_Tooltip_modifier_nevermore_requiem_slow);
 });
 dotaTooltipsEnglish(function (err, data) {
   if (err) {
