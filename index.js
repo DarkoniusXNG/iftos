063abe04c8ea3e6ad457e26ab62e373d35369d12
Darko V <darko1290@gmail.com>
2023-07-10 22:11:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,70 +5,82 @@ const valveKV = require('valve-kv');
 
 function dotaAbilities (cb) {
   request.get({
-    url: 'https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/npc_abilities.txt'
+    url: 'https://raw.githubusercontent.com/arcadia-redux/dota_vpk_updates/main/scripts/npc/npc_abilities.txt'
   }, function (err, result) {
     if (err) {
       return cb(err);
     }
-    const abilities1 = parseKV(result.body.replace(/redirect_range_scepter/g, '"redirect_range_scepter"'));
-    // const abilities2 = VDF.parse(result.body);
-    // const abilities3 = valveKV.deserialize(result.body);
+    // const bug1 = result.body.substring(1843800, 1843821);
+    // const bodyModified1 = result.body.replace(bug1, '"value"	"20 40 60 80"');
+    const bodyModified1 = result.body.replace(/(\n\s+)([a-zA-Z-_]+)(\s+"[^"]+")/g, '$1"$2"$3');
+    const abilities1 = parseKV(bodyModified1);
+    const abilities2 = VDF.parse(result.body);
+    const abilities3 = valveKV.deserialize(result.body);
+
     cb(null, abilities1);
+    // cb(null, abilities2);
+    // cb(null, abilities3);
   });
 }
 
 function dotaItems (cb) {
   request.get({
-    url: 'https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/items.txt'
+    url: 'https://raw.githubusercontent.com/arcadia-redux/dota_vpk_updates/main/scripts/npc/items.txt'
   }, function (err, result) {
     if (err) {
       return cb(err);
     }
     const items1 = parseKV(result.body); // parse-kv doesn't parse item_trident correctly
-    // const items2 = VDF.parse(result.body);
-    // const items3 = valveKV.deserialize(result.body);
+    const items2 = VDF.parse(result.body);
+    const items3 = valveKV.deserialize(result.body);
+
     cb(null, items1);
+    // cb(null, items2);
+    // cb(null, items3);
   });
 }
 
 function dotaHeroes (cb) {
   request.get({
-    url: 'https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/npc_heroes.txt'
+    url: 'https://raw.githubusercontent.com/arcadia-redux/dota_vpk_updates/main/scripts/npc/npc_heroes.txt'
   }, function (err, result) {
     if (err) {
       return cb(err);
     }
     const heroes1 = parseKV(result.body);
-    // const heroes2 = VDF.parse(result.body);
-    // const heroes3 = valveKV.deserialize(result.body);
+    const heroes2 = VDF.parse(result.body);
+    const heroes3 = valveKV.deserialize(result.body);
+
     cb(null, heroes1);
   });
 }
 
 function abilityTooltipsEnglish (cb) {
   request.get({
-    url: 'https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/resource/localization/abilities_english.txt'
+    url: 'https://raw.githubusercontent.com/arcadia-redux/dota_vpk_updates/main/resource/localization/abilities_english.txt'
   }, function (err, result) {
     if (err) {
       return cb(err);
     }
-    const abilitytooltips1 = parseKV(result.body.replace(/" and turn rate reduced by %dMODIFIER_PROPERTY_TURN_RATE_PERCENTAGE%%%\./g, ' and turn rate reduced by %dMODIFIER_PROPERTY_TURN_RATE_PERCENTAGE%%%."'));
-    // const abilitytooltips2 = VDF.parse(result.body.replace(/" and turn rate reduced by %dMODIFIER_PROPERTY_TURN_RATE_PERCENTAGE%%%\./g, ' and turn rate reduced by %dMODIFIER_PROPERTY_TURN_RATE_PERCENTAGE%%%."'));
-    // const abilitytooltips3 = valveKV.deserialize(result.body); // valve-kv ignores text outside of quotes
+    const abilitytooltips1 = parseKV(result.body);
+    const abilitytooltips2 = VDF.parse(result.body);
+    const abilitytooltips3 = valveKV.deserialize(result.body); // valve-kv ignores text outside of quotes
+
     cb(null, abilitytooltips1);
   });
 }
 
 function dotaTooltipsEnglish (cb) {
   request.get({
-    url: 'https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/resource/localization/dota_english.txt'
+    url: 'https://raw.githubusercontent.com/arcadia-redux/dota_vpk_updates/main/resource/localization/dota_english.txt'
   }, function (err, result) {
     if (err) {
       return cb(err);
     }
     const dotatooltips1 = parseKV(result.body);
-    // const dotatooltips2 = VDF.parse(result.body);
-    // const dotatooltips3 = valveKV.deserialize(result.body);
+    const dotatooltips2 = VDF.parse(result.body);
+    const dotatooltips3 = valveKV.deserialize(result.body);
+
     cb(null, dotatooltips2);
   });
 }
@@ -80,6 +92,7 @@ dotaAbilities(function (err, data) {
   }
   // console.log(data.DOTAAbilities.abaddon_borrowed_time);
   // console.log(data.DOTAAbilities.invoker_forge_spirit.AbilityValues.spirit_hp);
+  // console.log(data.DOTAAbilities.arc_warden_magnetic_field.AbilityValues.attack_magic_damage);
 });
 dotaItems(function (err, data) {
   if (err) {
@@ -108,5 +121,5 @@ dotaTooltipsEnglish(function (err, data) {
     console.error(err);
     return;
   }
-  // console.log(data.lang.Tokens);
+  // console.log(data.lang.Language);
 });
